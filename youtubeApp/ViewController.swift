23bb48e8a33d64e64be6cdc6226f068f4b9a2f4d
Alberto Martinez <albon_marvel@hotmail.com>
2017-02-01 16:36:21 -0600

    Initial Commit

---
+++ youtubeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youtubeApp
+//
+//  Created by Desarrollo Uxi on 01/02/17.
+//  Copyright © 2017 Alberto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
